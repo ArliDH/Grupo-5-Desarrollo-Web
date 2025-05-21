34052e40165fd1aaf95368ed874a19ca12c1ff45
Brayan Adam Leanos Ortiz <adam7712462@gmail.com>
2025-05-21 16:54:08 -0400

    datatabase

---
+++ backend/db/conexion.php
@@ -0,0 +1,20 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$host = 'localhost';
+$dbname = 'correo_empresa';
+$user = 'root';
+$pass = '';
+
+$con = mysqli_connect($host, $user, $pass, $dbname);
+
+if(mysqli_connect_errno()){
+    die("Se produjo un error ".mysqli_connect_error());
+}
+else{
+    echo "Conexión exitosa a la base de datos.";
+}
+?>
